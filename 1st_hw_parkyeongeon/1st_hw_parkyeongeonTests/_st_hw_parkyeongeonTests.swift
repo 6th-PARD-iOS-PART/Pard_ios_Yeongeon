be830093d2c15e6aabf2a1feb79bb6c0f6fd317f
yeongeon <duddjs1646@handong.ac.kr>
2025-09-19 01:16:30 +0900

    Complete 1st homework

---
+++ 1st_hw_parkyeongeon/1st_hw_parkyeongeonTests/_st_hw_parkyeongeonTests.swift
@@ -0,0 +1,17 @@
+//
+//  _st_hw_parkyeongeonTests.swift
+//  1st_hw_parkyeongeonTests
+//
+//  Created by Taemin KIM on 9/19/25.
+//
+
+import Testing
+@testable import _st_hw_parkyeongeon
+
+struct _st_hw_parkyeongeonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
