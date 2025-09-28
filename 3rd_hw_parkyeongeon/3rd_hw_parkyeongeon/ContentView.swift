0a7f50db8ccb826e5888dce70ecd578dfa11324e
yeongeon <duddjs1646@handong.ac.kr>
2025-09-28 10:12:51 +0900

    create project: 3rd_hw

---
+++ 3rd_hw_parkyeongeon/3rd_hw_parkyeongeon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  3rd_hw_parkyeongeon
+//
+//  Created by Taemin KIM on 9/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3rd_hw_parkyeongeon/3rd_hw_parkyeongeon/_rd_hw_parkyeongeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  _rd_hw_parkyeongeonApp.swift
+//  3rd_hw_parkyeongeon
+//
+//  Created by Taemin KIM on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct _rd_hw_parkyeongeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
